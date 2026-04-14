db142c591a023e995fa62fad8fb60dfadf23f02d
DrakeDev23 <maccogoth@gmail.com>
2026-04-14 20:27:18 +0800

    remove build files after merge

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ed1a090948e0e0b778d716fd413575ff7821cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f450c581964d2fcad4e55ba4706efd215927793c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
